0990c24359330f5c3af06af8d9fb66e974d9c721
plexpt <plexpt@gmail.com>
2024-12-21 17:33:36 +0800

    Refactoring and optimizing code

--- data-updates.lua
+++ data-updates.lua
@@ -1,14 +1,8 @@
-
--- Ensure all labs accept the farming-science-pack
 for _, lab in pairs(data.raw["lab"]) do
     if lab.inputs then
+        -- Ensure all labs accept the farming-science-pack
         table.insert(lab.inputs, "farming-science-pack")
-    end
-end
-
--- Ensure all labs accept the farming-science-pack
-for _, lab in pairs(data.raw["lab"]) do
-    if lab.inputs then
+        -- Ensure all labs accept the farming-science-pack
         table.insert(lab.inputs, "cooking-science-pack")
     end
 end
