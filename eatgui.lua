1c8fbdb24e77eb69ae6d98d9eb4a41b53ee57e6c
plexpt <plexpt@gmail.com>
2024-12-22 09:49:32 +0800

    fix bug

--- eatgui.lua
+++ eatgui.lua
@@ -10,35 +10,35 @@ function eatgui.create_eat_gui(player, item_name)
     local frame = player.gui.screen.add {
         type = "frame",
         name = "eat_gui",
-        caption = {"foodtorio.eat-gui-caption", item_name = item_name},
+        caption = { "foodtorio.eat-gui-caption", item_name },
         direction = "vertical"
     }
 
     -- Set the GUI location near the player (example: offset by 300 pixels in both directions)
-    frame.location = {x = 300, y = 300}
+    frame.location = { x = 300, y = 300 }
 
     -- Add content to the GUI
     frame.add {
         type = "label",
-        caption = {"foodtorio.eat-gui-label-question", item_name = item_name}
+        caption = { "foodtorio.eat-gui-label-question", item_name }
     }
 
     frame.add {
         type = "button",
         name = "eat_1_" .. item_name,
-        caption = {"foodtorio.eat-gui-button-eat-1"}
+        caption = { "foodtorio.eat-gui-button-eat-1" }
     }
 
     frame.add {
         type = "button",
         name = "eat_10_" .. item_name,
-        caption = {"foodtorio.eat-gui-button-eat-10"}
+        caption = { "foodtorio.eat-gui-button-eat-10" }
     }
 
     frame.add {
         type = "button",
         name = "eat_25_" .. item_name,
-        caption = {"foodtorio.eat-gui-button-eat-25"}
+        caption = { "foodtorio.eat-gui-button-eat-25" }
     }
 end
 
