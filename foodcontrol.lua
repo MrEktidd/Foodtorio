e01dddce5e3e4b9c68abd2558563087445c1691a
plexpt <plexpt@gmail.com>
2024-12-22 10:13:19 +0800

    update locale

--- foodcontrol.lua
+++ foodcontrol.lua
@@ -33,7 +33,7 @@ function foodcontrol.consume_food(player_index, item_name)
             end
         end
     else
-        game.print({"foodtorio.food-item-not-found", item_name})
+        game.get_player(player_index).print({"foodtorio.food-item-not-found", item_name})
     end
 end
 
@@ -42,18 +42,18 @@ function foodcontrol.consume_multiple_food(player_index, item_name, amount)
         for _ = 1, amount do
             foodcontrol.consume_food(player_index, item_name)
         end
-
+        local player = game.get_player(player_index)
         -- Print final status message after consuming multiple items
         local player_hunger = storage.player_hunger_data[player_index]
-        game.print({"foodtorio.hunger-levels-after-consume-multiple", amount, item_name})
-        game.print({"foodtorio.hunger-overall", player_hunger.overall})
-        game.print({"foodtorio.hunger-dairy", player_hunger.dairy})
-        game.print({"foodtorio.hunger-fruits", player_hunger.fruits})
-        game.print({"foodtorio.hunger-veggies", player_hunger.veggies})
-        game.print({"foodtorio.hunger-carbs", player_hunger.carbs})
-        game.print({"foodtorio.hunger-meat", player_hunger.meat})
+        player.print({"foodtorio.hunger-levels-after-consume-multiple", amount, item_name})
+        player.print({"foodtorio.hunger-overall", player_hunger.overall})
+        player.print({"foodtorio.hunger-dairy", player_hunger.dairy})
+        player.print({"foodtorio.hunger-fruits", player_hunger.fruits})
+        player.print({"foodtorio.hunger-veggies", player_hunger.veggies})
+        player.print({"foodtorio.hunger-carbs", player_hunger.carbs})
+        player.print({"foodtorio.hunger-meat", player_hunger.meat})
     else
-        game.print({"foodtorio.invalid-consumption"})
+        player.print({"foodtorio.invalid-consumption"})
     end
 end
 
